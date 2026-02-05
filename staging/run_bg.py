898a135d088ae30590baad71a06e185e35a1af95
WhiteW00lf <mocha_cookie_crumble@proton.me>
2026-02-05 18:41:33 +0530

    refactored code

--- staging/run_bg.py
+++ staging/run_bg.py
@@ -1,27 +1,26 @@
 from google.cloud import bigquery
 from pathlib import Path
 
-client = bigquery.Client()
-dataset_id = "deportfolio-486507.pollution_data"
-fct_path = Path(__file__).parent.parent /"facts" /"create_fact_tables.sql"
-dim_path = Path(__file__).parent.parent /"facts" /"create_dim_tables.sql"
+def make_tables():
+    client = bigquery.Client()
+    dataset_id = "deportfolio-486507.pollution_data"
+    fct_path = Path(__file__).parent.parent /"facts" /"create_fact_tables.sql"
+    dim_path = Path(__file__).parent.parent /"facts" /"create_dim_tables.sql"
 
-with open("create_stg_pollution.sql", "r") as f:
-    create_table_query = f.read()
-    job = client.query(create_table_query)
-    job.result()
-    print("Staging table created successfully.")
+    with open("create_stg_pollution.sql", "r") as f:
+        create_table_query = f.read()
+        job = client.query(create_table_query)
+        job.result()
+        print("Staging table created successfully.")
 
+    with open(fct_path, "r") as f:
+        create_table_query = f.read()
+        job = client.query(create_table_query)
+        job.result()
+        print("Facts table created successfully.")
 
-with open(fct_path, "r") as f:
-    create_table_query = f.read()
-    job = client.query(create_table_query)
-    job.result()
-    print("Facts table created successfully.")
-
-
-with open(dim_path, "r") as f:
-    create_table_query = f.read()
-    job = client.query(create_table_query)
-    job.result()
-    print("Dimension table created successfully.")
+    with open(dim_path, "r") as f:
+        create_table_query = f.read()
+        job = client.query(create_table_query)
+        job.result()
+        print("Dimension table created successfully.")
