dd2644100df4f5c78e4e2e506c55efe77c483d29
WhiteW00lf <mocha_cookie_crumble@proton.me>
2026-02-05 16:12:55 +0530

    added fact table code

--- staging/run_bg.py
+++ staging/run_bg.py
@@ -1,10 +1,19 @@
 from google.cloud import bigquery
+from pathlib import Path
 
 client = bigquery.Client()
 dataset_id = "deportfolio-486507.pollution_data"
+fct_path = Path(__file__).parent.parent /"facts" /"create_fact_tables.sql"
 
-with open("create_stg_pollution.sql", "r") as f:
+# with open("create_stg_pollution.sql", "r") as f:
+#     create_table_query = f.read()
+#     job = client.query(create_table_query)
+#     job.result()
+#     print("Staging table created successfully.")
+
+
+with open(fct_path, "r") as f:
     create_table_query = f.read()
     job = client.query(create_table_query)
     job.result()
-    print("Staging table created successfully.")
+    print("Facts table created successfully.")
