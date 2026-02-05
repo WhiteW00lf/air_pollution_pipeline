23fb517ff0aa6c97999f1584c3a30a278934bc27
WhiteW00lf <mocha_cookie_crumble@proton.me>
2026-02-05 16:28:12 +0530

    added dim table code

--- staging/run_bg.py
+++ staging/run_bg.py
@@ -4,12 +4,13 @@ from pathlib import Path
 client = bigquery.Client()
 dataset_id = "deportfolio-486507.pollution_data"
 fct_path = Path(__file__).parent.parent /"facts" /"create_fact_tables.sql"
+dim_path = Path(__file__).parent.parent /"facts" /"create_dim_tables.sql"
 
-# with open("create_stg_pollution.sql", "r") as f:
-#     create_table_query = f.read()
-#     job = client.query(create_table_query)
-#     job.result()
-#     print("Staging table created successfully.")
+with open("create_stg_pollution.sql", "r") as f:
+    create_table_query = f.read()
+    job = client.query(create_table_query)
+    job.result()
+    print("Staging table created successfully.")
 
 
 with open(fct_path, "r") as f:
@@ -17,3 +18,10 @@ with open(fct_path, "r") as f:
     job = client.query(create_table_query)
     job.result()
     print("Facts table created successfully.")
+
+
+with open(dim_path, "r") as f:
+    create_table_query = f.read()
+    job = client.query(create_table_query)
+    job.result()
+    print("Dimension table created successfully.")
