eecd072d543f4f541d0c2d30954f3527a60f69f9
WhiteW00lf <mocha_cookie_crumble@proton.me>
2026-02-05 15:32:49 +0530

    created staging table

---
+++ staging/create_stg_pollution.sql
@@ -0,0 +1,21 @@
+CREATE EXTERNAL TABLE IF NOT EXISTS `deportfolio-486507.pollution_data.stg_pollution_data` (
+
+    created_at TIMESTAMP ,
+    city STRING ,
+    aqi FLOAT64 ,
+    pm25 FLOAT64 ,
+    pm10 FLOAT64 ,
+    o3 FLOAT64 ,
+    no2 FLOAT64 
+
+
+
+
+)
+OPTIONS (
+    format = 'CSV',
+    uris = ['gs://pollution_raw/raw/*/pollution_data.csv'],
+    skip_leading_rows = 1,
+    field_delimiter = ','
+
+);
